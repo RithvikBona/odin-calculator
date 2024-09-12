308552f2ee87c3f91db9b12e1087e1a37dfdba18
Rithvik Bonagiri <rithvik.bonagiri@gmail.com>
2024-09-11 22:03:39 -0400

    Fix UI and Add Past Equation Functionality

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let num1 = null;
 let num2 = null;
 let lockScreen = false;
 let isDecimal = false;
-let hitOp = false;
-let opToChange = null;
 
 
 let add = (a,b) => a+b;
@@ -50,6 +48,26 @@ for(const element of numButtons) {
             currOperator = opToChange;
             hitOp = false;
             opToChange = null;
+            //find out the operator to display it to full
+            let opString = "null";
+            switch(currOperator) {
+                case add:
+                    opString = "+";
+                    break;
+                case subtract:
+                    opString = "-";
+                    break;
+                case multiply:
+                    opString = "x";
+                    break;
+                case divide:
+                    opString = "/";
+                    break;
+                case power:
+                    opString = "^";
+                    break;
+            }
+            displayFull.textContent = displayCurrent.textContent + " " + opString + " ";
             displayCurrent.textContent = element.textContent;
         } else {
             //check if number too big to add more
@@ -63,7 +81,8 @@ for(const element of numButtons) {
     });
 };
 
-
+let hitOp = false;
+let opToChange = null;
 //let noNumsAllowed = false;
 
 function operatorListener(op) {
@@ -75,33 +94,8 @@ function operatorListener(op) {
     //we can still change until we lock in the operator using numbers
     if(currOperator == null) {
         num1 = parseFloat(displayCurrent.textContent);
-        //find out the operator to display it to full
-        let opString = "null";
-        opToChange = op;
-        switch(op) {
-            case add:
-                opString = "+";
-                break;
-            case subtract:
-                opString = "-";
-                break;
-            case multiply:
-                opString = "x";
-                break;
-            case divide:
-                opString = "/";
-                break;
-            case power:
-                opString = "^";
-                break;
-        }
-        // if alr hit operator delete it from the full statement
-        let currString = displayFull.textContent;
-        if(hitOp) {
-            currString = currString.slice(0, -3);
-        }
-        displayFull.textContent = currString + " " + opString + " ";
         hitOp = true;
+        opToChange = op;
     } else {
         //we are doing calculation
         num2 = parseFloat(displayCurrent.textContent);
